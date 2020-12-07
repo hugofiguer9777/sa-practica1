4a1102de8a0b02f6ba8da15f672ad0efb17763d4
Hugo Figueroa <hugofiguer97@gmail.com>
2020-12-07 05:11:13 -0600

    Falla test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Aprende React
+          Prueba React
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('Test prueba1', () => {
   render(<App />);
-  const linkElement = screen.getByText(/practica #1/i);
+  const linkElement = screen.getByText(/falla forzada/i);
   expect(linkElement).toBeInTheDocument();
 });
