d9fb5c50355ecf48254944706cf58e5a6e18a0b0
hugofiguer9777 <58827958+hugofiguer9777@users.noreply.github.com>
2020-12-07 17:00:58 -0600

    Falla forzada

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           Practica #1 SA - Diciembre 2020
         </p>
         <p>
-          Hugo René Figueroa Castillo - 20103840
+          FALLA SIN DESPLEGAR
         </p>
       </header>
     </div>
